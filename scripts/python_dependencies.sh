c19b62ba61fe8bf43a0117d4a9f69c637d60b479
enigma <enigma@Enigmas-MacBook-Pro.local>
2023-11-09 07:15:28 +0300

    script to install os depencies

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-#virtualenv /home/ubuntu/truckman_env
+virtualenv /home/ubuntu/truckman_env
 source /home/ubuntu/truckman_env/bin/activate
-#pip install -r /home/ubuntu/truckman/requirements.txt 
+pip install -r /home/ubuntu/truckman/requirements.txt 
